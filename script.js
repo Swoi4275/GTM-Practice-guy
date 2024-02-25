1c7770ffe79108c8d204b46f9dafad6245235eeb
Swoi4275 <90369744+Swoi4275@users.noreply.github.com>
2024-02-25 08:55:08 -0500

    This are the Files for a multi page application
    As the single Page application did not have GTM code in all the pages, I have created a simple three page application so that all the pages will have the GTM code in it. 
    
    Signed-off-by: Swoi4275 <90369744+Swoi4275@users.noreply.github.com>

---
+++ script.js
@@ -0,0 +1,18 @@
+
+function goBack() {
+    window.location.href = 'index.html';
+}
+
+function goToLearn() {
+    window.location.href = 'learn.html';
+}
+
+function goToCongratulations() {
+    window.location.href = 'congrats.html';
+}
+
+function trackClick() {
+    alert('Button clicked!');
+    // Here you would push an event to the dataLayer for GTM
+    // dataLayer.push({'event': 'button_click'});
+}
